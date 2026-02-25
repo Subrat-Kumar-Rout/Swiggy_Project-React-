3d0fdfcd44d00bf2822952c78b83bc47fd13cda0
Subrat Kumar Rout <subratrout986@gmail.com>
2026-02-25 23:39:48 +0530

    Restcard file updated

--- src/component/RestCard.js
+++ src/component/RestCard.js
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 export default function RestCard({restInfo}){
     return(
         <>
-        
         <img className="w-70 h-45 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/" + restInfo?.info?.cloudinaryImageId}></img>
-        
         </>
     )
 }
